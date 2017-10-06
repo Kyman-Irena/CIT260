52ac833fab72b7eef038115e9795e7c01412fcaa
adamhawkins99 <32527229+adamhawkins99@users.noreply.github.com>
2017-10-06 13:45:20 -0700

    Added people for Adam's individual assignment

--- AdamsIndividualAssignment/src/adamsindividualassignment/People.java
+++ AdamsIndividualAssignment/src/adamsindividualassignment/People.java
@@ -5,10 +5,57 @@
  */
 package adamsindividualassignment;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  *
  * @author adam
  */
-public class People {
+public class People implements Serializable {
     
+    private String QuantityOfPeople;
+    private String people;
+    
+    public People() {
+    }
+    
+    public String getQuantityOfPeople(){
+        return QuantityOfPeople;
+        
+    }
+    public void setQuantityOfPeople (String people) {
+        this.QuantityOfPeople = people;
+    }
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.QuantityOfPeople);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final People other = (People) obj;
+        if (!Objects.equals(this.QuantityOfPeople, other.QuantityOfPeople)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" + "QuantityOfPeople=" + people + '}';
+    }
 }
+
