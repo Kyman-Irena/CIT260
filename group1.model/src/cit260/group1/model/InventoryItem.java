92075fbe296eb4df33ee3b96cef0cbe2a9df1a50
SeanErin <SeanErin@SeanErin-PC>
2017-10-07 22:51:07 -0600

    Erin Smith 10/7/2017 Just finished working on InventoryItem class.

--- group1.model/src/cit260/group1/model/InventoryItem.java
+++ group1.model/src/cit260/group1/model/InventoryItem.java
@@ -6,73 +6,37 @@
 package cit260.group1.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  *
- * @author admin
+ * @author Erin
  */
-public class Player implements Serializable {
-    
-    private String name;
-    private ArrayList<Game> games = new ArrayList<Game>();
-    
-    public Player() {
-    }
-    
-    
+public class InventoryItem implements Serializable{
+       private int quantityInStock;
+       private int requiredAmount;
 
-    public String getName() {
-        return name;
+    public InventoryItem() {
     }
 
-    public void setName(String name) {
-        this.name = name;
+       
+       
+    public int getQuantityInStock() {
+        return quantityInStock;
     }
 
-    public ArrayList<Game> getGames() {
-        return games;
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
     }
 
-    public void setGames(ArrayList<Game> games) {
-        this.games = games;
-    }
-    
-    
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 13 * hash + Objects.hashCode(this.name);
-        return hash;
+    public int getRequiredAmount() {
+        return requiredAmount;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Player other = (Player) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" + "name=" + name + '}';
+    public void setRequiredAmount(int requiredAmount) {
+        this.requiredAmount = requiredAmount;
     }
 
     
-    
 
-    
+       
 }
